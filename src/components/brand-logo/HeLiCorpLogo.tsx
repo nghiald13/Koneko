3151263055a95eaf7be1ae5941492d383219156c
nghiald13 <nghia.ld13@gmail.com>
2026-07-04 12:33:43 +0700

    fix: Fix HeLiCorp Logo render for light theme

--- src/components/brand-logo/HeLiCorpLogo.tsx
+++ src/components/brand-logo/HeLiCorpLogo.tsx
@@ -1,20 +1,26 @@
 'use client'
 
-import { useTheme } from "next-themes"
 import Image from "next/image"
 
 const HeLiCorpLogo = () => {
-    const { theme } = useTheme()
     return (
         <div className="relative sm:h-7 sm:w-32">
             <Image
-                src={`/images/${theme === 'light' ? "HeLiCorp-B-bg.webp" : "HeLiCorp-W-bg.webp"}`}
-                alt="Helicorp Logo"
+                src="/images/HeLiCorp-B-bg.webp"
+                alt="Helicorp Logo Light"
                 width={112}
                 height={28}
                 priority
-                style={{ height: 'auto' }}
-                className="w-28 h-auto sm:w-32 object-contain"
+                className="w-28 h-auto sm:w-32 object-contain dark:hidden"
+            />
+
+            <Image
+                src="/images/HeLiCorp-W-bg.webp"
+                alt="Helicorp Logo Dark"
+                width={112}
+                height={28}
+                priority
+                className="hidden w-28 h-auto sm:w-32 object-contain dark:block"
             />
         </div>
     )
